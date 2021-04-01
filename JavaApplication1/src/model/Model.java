060e22e82616881885f978ccc762dc3cd6545a54
Rafael Bassan <42983722+Rafabss@users.noreply.github.com>
2021-04-01 20:42:25 -0300

    COMIT ORGANIZADO

--- JavaApplication1/src/model/Model.java
+++ JavaApplication1/src/model/Model.java
@@ -10,10 +10,27 @@ public class Model {
 	
 	static List<Ponto> pontos = new ArrayList<>();
 	{
-		pontos.add(new Ponto(250, 250));
+            int i = 4;
+            
+            if(i == 4){
+                pontos.add(new Ponto(250, 250));
 		pontos.add(new Ponto(350, 250));
 		pontos.add(new Ponto(350, 350));
 		pontos.add(new Ponto(250, 350));
+            }
+            if(i == 6){
+            
+            for(int x = 0;x<6;x++){
+                pontos.add(new Ponto(250, 250));
+                for(int o = 0;o<6;o++){
+                    pontos.add(new Ponto(350, 250));
+                }
+            }
+            
+            
+            }
+            
+
 	};
 	
 	Desenho desenho = new Desenho(pontos);
